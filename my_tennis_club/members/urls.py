005694f871ccd9aa2211e2646bcec256347895bb
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-06-25 20:00:03 +0200

    nefunguje

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('members/', views.members, name='members'),
+    path('new_page/', views.new_page, name='new_page'),
 ]
 
--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+def members_old(request):
+    return HttpResponse("Hello world!")
 
 def members(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
+
+def new_page(request):
+    template = loader.get_template('new_page.html')
+    return HttpResponse(template.render())
