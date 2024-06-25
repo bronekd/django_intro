fedbdd6f1066b329f2d1e0c188d7523f447bf624
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-06-25 20:49:31 +0200

    Přidání vlastních stránek

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('members/', views.members, name='members'),
     path('new_page/', views.new_page, name='new_page'),
+    path('stranka_b/', views.stranka_b, name='stranka_b'),
+    path('druha_stranka/', views.druha_stranka, name='druha_stranka')
 ]
+
--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -8,11 +8,18 @@ def members_old(request):
 
 
 def members(request):
-  template = loader.get_template('myfirst.html')
-  return HttpResponse(template.render())
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
 
 
 def new_page(request):
-  template = loader.get_template('new_page.html')
-  return HttpResponse(template.render())
+    template = loader.get_template('new_page.html')
+    return HttpResponse(template.render())
 
+def stranka_b(request):
+    template = loader.get_template('stranka_b.html')
+    return HttpResponse(template.render())
+
+def druha_stranka(request):
+    template = loader.get_template('druha_stranka.html')
+    return HttpResponse(template.render())
